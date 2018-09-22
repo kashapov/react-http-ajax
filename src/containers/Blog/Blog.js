9c7143342f892ec1ff434d3c2f361f3f5a85e12a
Andrey Kashapov <kashapovandrey@gmail.com>
2018-09-22 16:02:02 +0300

    transforming Data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -15,13 +15,20 @@ class Blog extends Component {
     axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(response => {
         //console.log(response);
-        this.setState({posts: response.data});
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'Max'
+          };
+        });
+        this.setState({posts: updatedPosts});
       });
   }
 
   render() {
     const posts = this.state.posts.map(post => {
-      return <Post keu={post.id} title={post.title} />
+      return <Post keu={post.id} title={post.title} author={post.author}/>
     });
     return (
       <div>
