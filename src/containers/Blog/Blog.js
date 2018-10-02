6a8b3e80efbaf727e02bd9f2af0d531727c9d32c
Andrey Kashapov <kashapovandrey@gmail.com>
2018-10-02 21:42:43 +0300

    working with Guards

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
+state = {
+  auth: false
+}
+
   render() {
     return (
       <div className="Blog">
@@ -32,8 +36,9 @@ class Blog extends Component {
         <Route path="/" render={() => <h1>Other info</h1>} />*/}
 
         <Switch>          
-          <Route path="/new-post" component={NewPost} />
+          {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
           <Route path="/posts" component={Posts} />
+          <Redirect from="/" to="/posts" />
         </Switch>
 
       </div>
