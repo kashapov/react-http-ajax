51ef6aca8d00fda7286b52ac1ba9e0dc10906503
Andrey Kashapov <kashapovandrey@gmail.com>
2018-10-02 21:48:00 +0300

    Handling 404 Not found

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -38,7 +38,8 @@ state = {
         <Switch>          
           {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
           <Route path="/posts" component={Posts} />
-          <Redirect from="/" to="/posts" />
+          <Route render={() => <h1>404 Not found</h1>} />
+          {/*<Redirect from="/" to="/posts" />*/}
         </Switch>
 
       </div>
